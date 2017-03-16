434f6d9c4a927b900ef102a06779e4846a9593d0
Joel Bell <joel.m.bell@gmail.com>
2017-03-16 08:41:58 -0700

    Adding Presentation

--- SetsDemo.playground/Pages/Custom Data Types.xcplaygroundpage/Contents.swift
+++ SetsDemo.playground/Pages/Custom Data Types.xcplaygroundpage/Contents.swift
@@ -3,9 +3,14 @@
  */
 
 
-struct Person: Hashable {
+class Person: Hashable {
     var name: String
     var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
     
     public static func ==(lhs: Person, rhs: Person) -> Bool {
         return lhs.hashValue == rhs.hashValue
@@ -18,16 +23,24 @@ struct Person: Hashable {
     }
 }
 
-let joel = Person(name: "Joel", age: 29)
+var joel = Person(name: "Joel", age: 29)
+var joel2 = joel
+
+joel.name = "Frank"
+
+joel.name
+joel2.name
+
 let frank = Person(name: "William", age: 35)
 let billybob = Person(name: "Billy Bob", age: 43)
 let charles = Person(name: "Charles", age: 46)
 
 var set: Set = [joel, frank, billybob, charles]
 
-
 set.contains(Person(name: "Joel", age: 29))
 set.contains(Person(name: "joel", age: 29))
 set.contains(Person(name: "Joel", age: 30))
+
+
 
 
